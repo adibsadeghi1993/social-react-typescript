40f5ac520564a1ae3e82fef16bda2f6e4070ca88
adib sadeghi <adibsadeghi7272@gmail.com>
2021-12-31 15:33:17 +0330

    json-server was installed

---
+++ src/components/AddNewSocial.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const AddNewSocial = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AddNewSocial
