6378bcd55d1e4037e99df05a077a67d79bd17b10
adib sadeghi <adibsadeghi7272@gmail.com>
2021-12-31 20:48:18 +0330

    UserLinks was added to show all socialMedia

---
+++ src/components/UserLinks.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const UserLinks = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserLinks
