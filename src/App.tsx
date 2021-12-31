ddefd476ce891de3de6945b4c85f8a7cdf99b8ad
adib sadeghi <adibsadeghi7272@gmail.com>
2022-01-01 00:18:34 +0330

    collapse was successfully created with collapse component in mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { create } from "jss";
 import rtl from "jss-rtl";
+import Collapse from '@mui/material/Collapse';
 import {
   StylesProvider,
   jssPreset,
@@ -8,7 +9,7 @@ import {
   createTheme,
 } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-
+import Fade from '@mui/material/Fade';
 import Container from "@material-ui/core/Container/Container";
 import { Typography } from "@material-ui/core";
 import Box from "@mui/material/Box";
@@ -33,6 +34,7 @@ const useStyle = makeStyles({
 });
 
 export default function App() {
+  const [isShow, setIsShow] = React.useState(false);
   const classes = useStyle();
   return (
     <StylesProvider jss={jss}>
@@ -40,13 +42,20 @@ export default function App() {
         <CssBaseline />
         <div className="app">
           <Container className="container" maxWidth="md">
-            <Box sx={{display:"flex",mb:1,cursor:"pointer"}}>
+            <Box onClick={()=>setIsShow(!isShow)} sx={{display:"flex",mb:1,cursor:"pointer"}}>
               <AddIcon sx={{ color:"yellow" }}/>
               <Typography className={classes.add} variant="subtitle2">
                 افزوردن راه ارتباطی
               </Typography>
             </Box>
+            <Box sx={{backgroundColor:"green",mt:2}}>
+            <Collapse  in={isShow}>
             <AddNewSocial />
+            </Collapse>
+        
+      </Box>
+            
+           
           <UserLinks/>
           </Container>
         </div>
