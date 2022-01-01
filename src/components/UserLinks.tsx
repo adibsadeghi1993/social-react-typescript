ca63e26124a934a315f91d2049dfa8a8eb064ff5
adib sadeghi <adibsadeghi7272@gmail.com>
2022-01-01 16:10:26 +0330

    edit and delete icone was added

--- src/components/UserLinks.tsx
+++ src/components/UserLinks.tsx
@@ -3,6 +3,8 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "../hooks/useTypesSelector";
 import { getUserContacts } from "../redux/actions/actions";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import EditIcon from '@mui/icons-material/Edit';
 
 
 interface Props {}
@@ -19,10 +21,17 @@ console.log(state)
   return <div>
       {state.socials && <div>
           { state.socials.map((item)=>{
-               return <Box sx={{display:"flex",mt:2}} key={item.id}>
+               return <Box sx={{display:"flex",mt:2, justifyContent: 'space-between'}} key={item.id}>
+                   <Box sx={{display:"flex"}}>
                    <Typography variant="subtitle2">راه ارتباطی : {item.social_type}</Typography>
                    <Typography mr={2} variant="subtitle2"> لینک : {item.social_link}:</Typography>
                    <Typography mr={2}  variant="subtitle2">آی دی : {item.social_id}</Typography>
+                   </Box>
+                   <Box>
+                   <Box component="span"><EditIcon/></Box>
+                       <Box component="span" sx={{mr:2}}><DeleteForeverIcon/></Box>
+                      
+                   </Box>
                </Box >
            })}
           
